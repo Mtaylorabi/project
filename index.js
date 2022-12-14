eb267a937342d8e69146f31cf724aaa2c200c9fc
Mtaylorabi <marius.taylor-ext@ab-inbev.com>
2022-12-14 10:04:24 -0300

    v6

--- index.js
+++ index.js
@@ -17,23 +17,21 @@ function checkInputs() {
 function api() {
   const existingResponseElements = document.querySelectorAll('.response-text');
   existingResponseElements.forEach(el => el.remove());
-  const lado1 = document.querySelector('.lado1').className;
-  const lado2 = document.querySelector('.lado2').className;
-  const lado3 = document.querySelector('.lado3').className;
-  const queryString = `?lado1=${lado1}&lado2=${lado2}&lado3=${lado3}`;
-
-  fetch(`https://636e8cdebb9cf402c804b9f3.mockapi.io/triangulo${queryString}`)
+  const lado1 = document.querySelector('.lado1').value;
+  const lado2 = document.querySelector('.lado2').value;
+  const lado3 = document.querySelector('.lado3').value;
+  fetch(`http://localhost:3000/triangulo/${lado1}/${lado2}/${lado3}`)
     .then(res => res.json())
     .then(data => {
       const responseElement = document.createElement('h');
-      responseElement.innerText = data[0].tipo;
+      responseElement.innerText = data.tipo;
       responseElement.classList.add('response-text');
       if (responseElement.innerText === 'equilátero') {
-        responseElement.style.color = 'orange';
+        responseElement.style.color = '#FFA500';
       } else if (responseElement.innerText === 'isósceles') {
-        responseElement.style.color = 'blue';
+        responseElement.style.color = '#0000FF';
       } else if (responseElement.innerText === 'escaleno') {
-        responseElement.style.color = 'lightgreen';
+        responseElement.style.color = '#90EE90';
       }
       document.querySelector('#calcularTriangulo').insertAdjacentElement('beforeend', responseElement);
     });
